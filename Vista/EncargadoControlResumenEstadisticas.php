7a58f40d5a76f45c1f990151caf6e28712779002
Michi MS <63216518+michims@users.noreply.github.com>
2021-12-07 07:41:32 -0600

    Add files via upload

---
+++ Vista/EncargadoControlResumenEstadisticas.php
@@ -0,0 +1,592 @@
+<?php
+ session_status() === PHP_SESSION_ACTIVE ? TRUE : session_start();
+
+ if($_SESSION['id']==''){
+  header('Location: Principal.php');
+}else{
+$id=$_SESSION['id'];
+$rol= $_SESSION['Rol'];
+$depa= $_SESSION['departamento'];
+
+if($rol=='EmpleadoDep'){
+  $visibleEmpleadoDep='auto';
+  $visibleEncargadoDep='none';
+  $visibleFiscalizador='none';
+  $visibleEncargadoControlInt='none';
+  $graficoEncargadoControlInt='none';
+}
+
+if($rol=='EncargadoDep'){
+  $visibleEmpleadoDep='none';
+  $visibleEncargadoDep='auto';
+  $visibleFiscalizador='none';
+  $visibleEncargadoControlInt='none';
+  $graficoEncargadoControlInt='none';
+}
+
+if($rol=='Fiscalizador'){
+  $visibleEmpleadoDep='none';
+  $visibleEncargadoDep='none';
+  $visibleFiscalizador='auto';
+  $visibleEncargadoControlInt='none';
+  $graficoEncargadoControlInt='none';
+}
+
+if($rol=='EncargadoControlInterno'){
+  $visibleEmpleadoDep='none';
+  $visibleEncargadoDep='none';
+  $visibleFiscalizador='none';
+  $visibleEncargadoControlInt='auto';
+  $graficoEncargadoControlInt='block';
+}
+
+require '../Modelo/ConsultasModel.php';
+require_once '../Config.php';
+$objconfig = new Config();
+$objsm =  new ConsultasModel($objconfig);
+$infotabla="";
+$infotabla=$objsm->ResultadosDepartamento();
+$departamento=$objsm->ComboRegistroUsuario();
+$departamento2=$objsm->ComboRegistroUsuario();
+$periodos=$objsm->PeriodosFormulario();
+
+$infocombo=$objsm->PeriodosFormularioCombo();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="style/styleGeneral.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="plugins/jnoty.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="plugins/jnoty.min.css"> 
+    <script src="https://code.highcharts.com/stock/highstock.js" type="text/javascript"></script>
+    <script src="https://code.highcharts.com/highcharts-more.js" type="text/javascript"></script>
+    <script src="https://code.highcharts.com/highcharts-3d.js" type="text/javascript"> </script>
+    <script src="https://code.highcharts.com/modules/data.js" type="text/javascript"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js" type="text/javascript"></script>
+    <script src="https://code.highcharts.com/modules/funnel.js" type="text/javascript"></script>
+    <script src="https://code.highcharts.com/modules/annotations.js" type="text/javascript"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js" type="text/javascript"></script>
+    <script src="https://code.highcharts.com/modules/solid-gauge.js" type="text/javascript"></script>
+ 
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="sidebar">
+    <div style="text-align: center;">
+    <a href="Perfil.php">
+      <img src="img/logo.png" alt="logo" -o-object-fit: cover width="50%" height="100%">
+</a>
+</div>
+<ul class="nav-links" style="display:<?php echo $visibleEmpleadoDep ?>">
+        <li>
+        </li>
+    <li>
+          <a href="MenuFormulario.php">
+            <i class='bx bxs-file' ></i>
+            <span class="links_name">Formulario Autoevaluación</span>
+          </a>
+        </li>
+        <li>
+          <a href="MenuResultados.php">
+            <i class='bx bxs-bar-chart-alt-2' ></i>
+            <span class="links_name">Resultados Departamento</span>
+          </a>
+        </li>
+    
+        <li>
+        <a href="Ayuda.php">
+            <i class='bx bx-help-circle' ></i>
+            <span class="links_name">Ayuda</span>
+          </a>
+        </li>
+        <li class="log_out">
+          <a href="Principal.php">
+            <i class='bx bxs-door-open'></i>
+            <span class="links_name">Salir</span>
+          </a>
+        </li>
+      </ul>
+
+    <ul class="nav-links" style="display:<?php echo $visibleEncargadoDep ?>">
+        <li>
+        </li>
+    <li>
+          <a href="MenuFormulario.php">
+            <i class='bx bxs-file' ></i>
+            <span class="links_name">Formulario Autoevaluación</span>
+          </a>
+        </li>
+        <li>
+          <a href="MenuResultados.php">
+            <i class='bx bxs-bar-chart-alt-2' ></i>
+            <span class="links_name">Resultados Departamento</span>
+          </a>
+        </li>
+        <li>
+        <a href="AsignacionEvidencias.php">
+            <i class='bx bxs-group' ></i>
+            <span class="links_name">Asignación de Evidencias</span>
+          </a>
+        </li> 
+        <li class="log_out">
+          <a href="Principal.php">
+            <i class='bx bxs-door-open'></i>
+            <span class="links_name">Salir</span>
+          </a>
+        </li>
+      </ul>
+
+      <ul class="nav-links"style="display:<?php echo $visibleFiscalizador ?>">
+        <li>
+        </li>
+        <li>
+          <a href="AdmiManejoFormulario.php">
+            <i class='bx bxs-file' ></i>
+            <span class="links_name">Manejo Formulario Autoevaluación</span>
+          </a>
+        </li>
+        <li>
+          <a href="AdmiManejoUsuarios.php">
+            <i class='bx bx-user' ></i>
+            <span class="links_name">Manejo de Usuarios</span>
+          </a>
+        </li>
+        <li>
+          <a href="RespuestaContacto.php">
+            <i class='bx bx-message-rounded-error' ></i>
+            <span class="links_name">Consultas</span>
+          </a>
+        </li>
+        <li class="log_out">
+          <a href="Principal.php">
+            <i class='bx bxs-door-open'></i>
+            <span class="links_name">Salir</span>
+          </a>
+        </li>
+      </ul>
+
+      <ul class="nav-links" style="display:<?php echo $visibleEncargadoControlInt ?>">
+        <li>
+        </li>
+        <li>
+          <a href="MenuRevisionRespuestasFormulario.php">
+            <i class='bx bxs-bar-chart-alt-2' ></i>
+            <span class="links_name">Evaluación de Resultados</span>
+          </a>
+        </li>
+        <li class="log_out">
+          <a href="Principal.php">
+            <i class='bx bxs-door-open'></i>
+            <span class="links_name">Salir</span>
+          </a>
+        </li>
+      </ul>
+  </div>
+    <section class="home-section">
+    <nav>
+      <div class="sidebar-button">
+        <i class='bx bx-menu sidebarBtn'></i>
+        <span class="dashboard"></span>
+      </div>
+    </nav>
+
+<div class="home-content">
+    
+    
+  <div class="contenido" style="text-align:center;">
+  
+  <h3>Reporte de Formulario por Período</h3>
+         <br>
+
+      <select class="un " name="periodo" id="periodo" style="width:30%" required >
+        <option value="" disabled selected>Seleccione un período de evaluación</option>
+        <?php while ($row = mysqli_fetch_array($infocombo)){
+                        echo "<option value=". $row ['IdPeriodo'].">" . $row['FechaInicio']."/".$row['FechaFinal'] . "</option>";
+                        }
+                        ?>
+        </select>
+        <br>
+        <select class="un " name="opciondepa" id="opciondepa" style="width:30%" required >
+        <option value="" disabled selected>Seleccione un Departamento</option>
+        <?php while ($row = mysqli_fetch_array($departamento)){
+                        echo "<option value=". $row ['IdDepartamento'].">" . $row['NombreDepartamento'] . "</option>";
+                        }
+                        ?>
+        </select>
+        <br>
+
+        <button class="submit" type="button" onclick="Infoperiodo()"  align="center" style="width:20%">Buscar</button>
+        <br>
+        <br>
+        <div style="width:50%; margin-left:auto ;margin-right:auto" >
+
+<table name="tablaperiodo" id="tablaperiodo">
+<thead>
+<tr>
+  <th>Componente</th>
+  <th>Promedio</th>
+  <th></th>
+</tr>
+</thead><thead>
+</thead><tbody>
+</tbody>
+</table>
+              </div>
+  <br>
+  <hr>
+  <br>
+          <h3>Comparativa Promedio General Empresa</h3>
+          <br>
+          <table style="display:none" name="compgen" id="compgen">
+       <thead>
+        <tr>
+          <th>Periodo</th>
+          <th>Puntaje</th>
+        </tr>
+       </thead><thead>
+       </thead><tbody>
+       <?php while ($row = mysqli_fetch_array($periodos)){
+      echo "<tr><td>".$row["FechaInicio"]."/".$row["FechaFinal"]."</td><td>".$row["PromEmpresa"]."</td></tr>";
+       }
+?>
+       </tbody>
+          </table>
+
+         
+  <div id="containerperiodos" name="containerperiodos" >
+    
+    </div>
+          <br>
+          <h3>Resultados formulario por Departamento</h3>
+          <br>
+          <table style="display:none" name="graficoDep" id="graficoDep">
+       <thead>
+        <tr>
+          <th>Departamento</th>
+          <th>Puntaje</th>
+        </tr>
+       </thead><thead>
+       </thead><tbody>
+       <?php while ($row = mysqli_fetch_array($infotabla)){
+      echo "<tr><td>".$row["NombreDepartamento"]."</td><td>".$row["Puntaje"]."</td></tr>";
+       }
+        ?>
+       </tbody>
+          </table>
+
+    
+    <div id="containerGraficosDptos" name="containerGraficosDptos" >
+    
+    </div>
+<hr>
+<br>
+<br>
+<h3>Resultados Formulario para Departamento por Componente</h3>
+          <br>
+    <form class="form1">
+      <select class="un " name="opcion" id="opcion" onchange="ReporteResultadosComponente(this);" required >
+        <option value="" disabled selected>Seleccione un departamento Departamento</option>
+        <?php while ($row = mysqli_fetch_array($departamento2)){
+                        echo "<option value=". $row ['IdDepartamento'].">" . $row['NombreDepartamento'] . "</option>";
+                        }
+                        ?>
+        </select>
+
+      </form>
+      <table style="display:none" name="barras" id="barras">
+       <thead>
+        <tr>
+          <th>Componente</th>
+          <th>Criterio No.1</th>
+          <th>Criterio No.2</th>
+          <th>Criterio No.3</th>
+          <th>Criterio No.4</th>
+        </tr>
+       </thead><thead>
+       </thead><tbody>
+      
+
+       </tbody>
+       </table>
+         <div id="containerGraficos"  name="containerGraficos">
+
+
+         </div>
+
+         <div id="popup1" class="overlay">
+	<div class="popup">
+		<center><h3>Desglose Información Componente</h3></center>
+		<a class="close" href="#">&times;</a>
+		<div class="content">
+			<br>
+      <br>
+      <table  style="width:50%;"name="tablaperiodogen" id="tablaperiodogen">
+      <thead>
+        <tr>
+          <th style="width:50%;"><div style="width:50%">Característica</div></th>
+          <th>Opción</th>
+          <th>Valor</th>
+        </tr>
+      </thead><thead>
+      </thead><tbody>
+      </tbody>
+    </table>
+     
+            
+        </form> 
+       
+      <br>
+		</div>
+	</div>
+  </div>
+  
+</div>
+
+
+  
+  </section>
+  </body>
+  <script>
+
+  $(function() {// cualquier cosa que se escriba dentro de esta función, se va a ejecutar apenas cargue la página
+    ReporteResultadoComparativo();
+    comparativaperiodos();//Cargamos el reporte comparativo apenas entramos a la página
+});
+
+function Infoperiodo(){
+            
+            var op1=$("#periodo").val();
+            var op2=$("#opciondepa").val();
+          $.ajax({
+            url: 'EncargadoControlResumenEstadisticasMetodo.php?tipo=filtroperiodo', 
+            type: 'POST',
+            data:{Periodo:op1,Departamento:op2} , 
+            success:function(response)
+            { if(response!=""){
+              $("#tablaperiodo > tbody").html("");
+              $("#tablaperiodo").append(response);
+              mensaje("Se han cargado los resultados del departamento para el período de evaluación","success")
+            }
+
+              else{
+              mensaje("No existen resultados para el departamento","info")
+              $("#tablaperiodo > tbody").html("");
+
+            }
+            }
+
+          });
+        }
+
+    function ReporteResultadosComponente(sel) {//Recibe this para poder sacar el value y el texto
+    var idDepartamento=sel.value;
+    var nomdepartamento= $(sel).find('option:selected').text();
+    $.ajax({
+        url: 'EncargadoControlResumenEstadisticasMetodo.php?tipo=otro', 
+        type: 'POST',
+        data:{iddepartamento:idDepartamento} , // The form with the file inputs.
+        success:function(response)
+        { 
+          $("#barras > tbody").html("");
+              $("#barras").append(response);
+              GraficoReporteResultadosComponentes(nomdepartamento);
+        }
+      });
+    }
+    function comparativaperiodos(){
+   var config=
+   {
+	"title": {
+		"text": "Comparativa por periodos"
+	},
+	"subtitle": {
+		"text": ""
+	},
+	"exporting": {},
+	"chart": {
+		"type": "line"
+	},
+	"series": [{
+		"name": "Column 2",
+		"turboThreshold": 0
+	}],
+	"plotOptions": {
+		"series": {
+			"animation": false
+		}
+	},
+	"data": {
+		"table": "compgen"
+	},
+	"yAxis": {
+		"title": {
+			"text": "Índice de Madurez"
+		}
+	},
+	"colors": ["#ff6f00", "#434348", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"]
+};
+
+$("#containerperiodos").highcharts(config);
+ }
+    
+  function GraficoReporteResultadosComponentes(nomdepartamento){
+      var config={// Guardamos todas la configuración del Grafico en una variable
+	"title": {
+		"text": "Resultados por Componente y Criterio"
+	},
+	"subtitle": {
+		"text": "Departamento "+ nomdepartamento
+	},
+	"exporting": {},
+	"chart": {
+		"type": "column",
+		"polar": false
+	},
+	"series": [{ // Especificamos las Series de datos que va a mostrar el gráfico
+		"name": "Criterio 1",
+		"turboThreshold": 0,
+		"marker": {}
+	}, {
+		"name": "Criterio 2",
+		"turboThreshold": 0
+	}, {
+		"name": "Criterio 3",
+		"turboThreshold": 0
+	}, {
+		"name": "Criterio 4",
+		"turboThreshold": 0
+	}],
+	"plotOptions": {
+		"series": {
+			"animation": false,
+			"dataLabels": {}
+		}
+	},
+	"data": {//Aquí agregamos la data que va a contener el gráfico, puede enviarsele como un CSV, Si armas un CSV exactamente al del ejemplo pero con la info de la base de datos, te funciona bien
+	"table":"barras"
+	},
+	"legend": {},
+	"colors": ["#7cb5ec", "#434348", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"],
+	"tooltip": {},
+	"lang": {},
+	"credits": {},
+  "yAxis": {
+    "title": {
+      "text": "Puntaje" //Texto del eje Y
+    }
+  }
+}
+      $("#containerGraficos").highcharts(config);//Creamos el Highcharts dentro del div llamado containerGRaficos con la configuración creada
+   
+
+
+    }
+
+    function ReporteResultadoComparativo(){
+      var config={
+	"title": {
+		"text": "Comparativo entre Departamentos"
+	},
+	"subtitle": {
+		"text": "Índice de Madurez"
+	},
+	"exporting": {},
+	"chart": {
+		"type": "column",
+		"margin": 75,
+		
+		"polar": false
+	},
+	"plotOptions": {
+		"column": {
+			"depth": 25
+		},
+		"series": {
+			"animation": false,
+			"dataLabels": {}
+		}
+	},
+	"series": [{ // Aqui se agregan las series que va a mostrar el gráfico en el eje Y
+		"name": "Puntaje",
+		"turboThreshold": 0,
+		"marker": {},
+		"color": "#f57c00"
+	}],
+	"data": {
+		"table": "graficoDep"
+	},
+	"yAxis": {
+		"title": {
+			"text": "Índice Promedio"
+		}
+	},
+	"legend": {},
+	"tooltip": {},
+	"lang": {},
+	"credits": {}
+}
+
+      $("#containerGraficosDptos").highcharts(config);//Creamos el Highcharts dentro del div llamado containerGRaficos con la configuración creada
+   
+  }
+
+    
+   let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function() {
+  sidebar.classList.toggle("active");
+  if(sidebar.classList.contains("active")){
+  sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
+}else
+  sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+}
+
+    function mensaje(msj,tipo){//tipos= success,info,danger,warning
+      $.jnoty(msj, {
+            sticky: false,
+            header: 'Review.me',
+            theme: 'jnoty-'+tipo,
+            icon: 'fa fa-check-circle fa-2x'
+          });
+
+    }
+    function  cargacomp(componente){
+
+InfoGen(componente);
+}
+function InfoGen(comp){
+      
+      var op=$("#opciondepa").val();
+    $.ajax({
+      url: 'ResumenEstadisticasEncargadoDepMetodo.php?tipo=General', 
+      type: 'POST',
+      data:{Opcion:op,componente:comp} , 
+      success:function(response)
+      { if(response!=""){
+     
+        $("#tablaperiodogen > tbody").html("");
+        $("#tablaperiodogen").append(response);
+        mensaje("Se han cargado los resultados del componente para el período de evaluación","success")
+      }
+
+        else{
+        mensaje("No existen resultados para el departamento","info")
+
+      }
+      }
+
+    });
+  }
+</script>     
+
+</html>
+
---
+++ Vista/EncargadoControlResumenEstadisticasMetodo.php
@@ -0,0 +1,84 @@
+<?php
+error_reporting(E_ALL ^ E_WARNING);
+
+$tipo=$_REQUEST["tipo"];
+
+if($tipo=="otro"){
+$depa=$_REQUEST["iddepartamento"];
+      require '../Modelo/ConsultasModel.php';
+      require_once '../Config.php';
+      $objconfig = new Config();
+      $objsm =  new ConsultasModel($objconfig);
+      $infotabla="";
+      $infotabla=$objsm->TablaResultadosForm($depa);
+       $ambiente="";
+       $riesgo="";
+       $control="";
+       $sistemas="";
+       $seguimiento="";
+       while ($row = mysqli_fetch_array($infotabla)){
+
+        if($row ['NombreComponente']=='Ambiente de Control'){
+          $ambiente=$ambiente.$row ['ValorOpcion'].";";
+        }
+        if($row ['NombreComponente']=='Valoración de Riesgo'){
+          $riesgo=$riesgo.$row ['ValorOpcion'].";";
+        }
+        if($row ['NombreComponente']=='Actividades de control'){
+          $control=$control.$row ['ValorOpcion'].";";
+        }
+        if($row ['NombreComponente']=='Sistemas de información'){
+          $sistemas=$sistemas.$row ['ValorOpcion'].";";
+        }
+        if($row ['NombreComponente']=='Seguimiento del Sistema de Control Interno'){
+          $seguimiento=$seguimiento.$row ['ValorOpcion'].";";
+        }
+       }
+       $ambiente2=explode(";",$ambiente);
+       $riesgo2=explode(";",$riesgo);
+       $control2=explode(";",$control);
+       $sistemas2=explode(";",$sistemas);
+       $seguimiento2=explode(";",$seguimiento);
+       $table="<tr><td>Ambiente de Control</td><td>".$ambiente2[0]."</td><td>".$ambiente2[1]."</td><td>".$ambiente2[2]."</td><td>".$ambiente2[3]."</td></tr>";
+       $table=$table."<tr><td>Valoración de Riesgo</td><td>".$riesgo2[0]."</td><td>".$riesgo2[1]."</td><td>".$riesgo2[2]."</td><td>".$riesgo2[3]."</td></tr>";
+       $table=$table."<tr><td>Actividades de control</td><td>".$control2[0]."</td><td>".$control2[1]."</td><td>".$control2[2]."</td><td>".$control2[3]."</td></tr>";
+       $table=$table."<tr><td>Sistemas de información</td><td>".$sistemas2[0]."</td><td>".$sistemas2[1]."</td><td>".$sistemas2[2]."</td><td>".$sistemas2[3]."</td></tr>";
+       $table=$table."<tr><td>Seguimiento del Sistema de Control Interno</td><td>".$seguimiento2[0]."</td><td>".$seguimiento2[1]."</td><td>".$seguimiento2[2]."</td><td>".$seguimiento2[3]."</td></tr>";
+
+       echo $table;
+      }
+
+
+      if($tipo=="filtroperiodo"){
+        $periodo=$_REQUEST["Periodo"];
+        $depa= $_REQUEST["Departamento"];
+        require '../Modelo/ConsultasModel.php';
+        require_once '../Config.php';
+        $objconfig = new Config();
+        $objsm =  new ConsultasModel($objconfig);
+        $result=$objsm->InfoComponentexPeriodo($periodo,$depa); 
+        $info2="";
+        $suma=0;
+        $cuenta=0;
+        while ($row = mysqli_fetch_array($result)){
+          $info2=$info2."<tr><td>". $row ['NombreComponente']."</td><td>". round($row['Promedio'],2)."</td><td><a href='#popup1' onclick='cargacomp(\"". $row ['NombreComponente']."\")' class='btnSelect' ><i class='bx bx-file-find bx-sm' ></i></a></td></tr>";
+            $suma=$suma+$row['Promedio'];
+            $cuenta=$cuenta+1;
+            }
+            if($cuenta>0){
+            $prom=$suma/$cuenta;
+            $color='#fffff';
+            if($prom<60){
+                $color='#EB092A';
+            }else{
+                $color='#9BEB09';
+            }
+            $info2=$info2."<tr style='background-color:".$color."'><td><b>Promedio Departamento</b></td><td><b>".$prom."</b></td><td></td></tr>";
+            }
+            echo $info2;
+    
+      
+      }
+
+
+       ?>
